8240d588d69feb48c9af6ec41d7ee7eb5dc5dc48
santosh <santosh_philip@yahoo.com>
2013-05-20 08:32:18 -0700

    dealing with weather.stat files

---
+++ csvstuff.py
@@ -0,0 +1,36 @@
+import csv
+txt = open('b.csv', 'r').read()
+lines = txt.splitlines()
+rows = [line.split(',') for line in lines]
+rows = [[float(cell) for cell in row] for row in rows]
+brows = [[row[4], row[6]] for row in rows if row[-1] == 0 and row[-2] == 0]
+csv_writer = csv.writer(open('brows.csv', 'w')).writerows(brows)
+
+drows = [[row[5], row[7]] for row in rows if row[-1] == 0 and row[-2] == 0]
+csv_writer = csv.writer(open('drows.csv', 'w')).writerows(drows)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# b0rows = [row for row in brows if row[0] == 0 and row[1] == 0]
+# csv_writer = csv.writer(open('brows.csv', 'w')).writerows(b0rows)
+# b0rows = [row for row in brows if row[0] != 0 and row[1] != 0]
+# csv_writer = csv.writer(open('brows.csv', 'w')).writerows(b0rows)
+# csv_writer = csv.writer(open('brows.csv', 'w')).writerows(b0rows)
+# b0rows = [row[0] - row[1] for row in brows if row[0] != 0 and row[1] != 0]
+# csv_writer = csv.writer(open('brows.csv', 'w')).writerows(b0rows)
+# b0rows = [[row[0] - row[1]] for row in brows if row[0] != 0 and row[1] != 0]
+# csv_writer = csv.writer(open('brows.csv', 'w')).writerows(b0rows)
+# b0rows = [row for row in brows if row[0] != 0 and row[1] != 0]
+# csv_writer = csv.writer(open('brows.csv', 'w')).writerows(b0rows)
+# hist

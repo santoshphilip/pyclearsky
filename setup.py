b5242bc4a5c3c99b4519e5cbd4b03d4820208ff1
Santosh Philip <santosh_philip@yahoo.com>
2018-04-24 12:31:38 -0700

    Bump version: 0.2.16 → 0.2.17

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/santoshphilip/pyclearsky',
-    version='0.2.16',
+    version='0.2.17',
     zip_safe=False,
 )

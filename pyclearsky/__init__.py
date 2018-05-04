a0013c0a0305bcaf1181defc706f02fd24dbc087
Santosh Philip <santosh_philip@yahoo.com>
2018-05-04 16:36:05 -0700

    Bump version: 0.2.17 → 0.3.0

--- pyclearsky/__init__.py
+++ pyclearsky/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Santosh Philip"""
 __email__ = 'santosh@example.com'
-__version__ = '0.2.17'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/santoshphilip/pyclearsky',
-    version='0.2.17',
+    version='0.3.0',
     zip_safe=False,
 )

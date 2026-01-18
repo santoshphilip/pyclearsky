8c3d4e0074632d32759068936987032ae63ff8e4
Santosh Philip <santosh@noemail.com>
2026-01-18 08:51:07 -0800

    Bump version to

--- pyclearsky/__init__.py
+++ pyclearsky/__init__.py
@@ -4,4 +4,11 @@
 
 __author__ = """Santosh Philip"""
 __email__ = 'santosh@example.com'
-__version__ = '0.3.0'
+# __version__ = '0.3.0'
+# src/your_package/__init__.py
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pyclearsky")   # must match [project] name =
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"                   # fallback for editable installs / dev
--- runningnotes.rst
+++ runningnotes.rst
@@ -2,6 +2,14 @@
 Running Notes for pyclearsky
 ============================
 
+
+2026-01-18
+==========
+
+bumping a version and installing
+
+- open an issue to include all the setup.py stuff in eppy that is not in the pyproject.toml file
+
 running on uv
 
 bump an update and publish to pypi
